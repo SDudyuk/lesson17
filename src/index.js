c77b44ddc0c76f4e2a083ba3e14e61932c68fcb1
SDudyuk <sdudyuk.o@gmail.com>
2023-10-23 22:07:24 +0300

    remove delay from log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function randomDelayPrint(message) {
   if (message.charAt(0)) {  
     const delay = Math.random()*1000;
-    console.log(message.charAt(0),delay);
+    console.log(message.charAt(0));
     setTimeout(() => {
       randomDelayPrint(message.substr(1))
     }, delay);
